7fe9c1cf64a13ec09473894f8f32566c9893fb57
Armennon <57568491+Armennon@users.noreply.github.com>
2019-11-13 23:38:35 +0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const util = require("./myUtils.js");
 
 const reader = require('readline-sync');
 
-console.log(util.isPrimeForRange(reader.questionInt("Input first number "), reader.questionInt("Input second number ")));
+console.log(util.arrayElements(util.isPrimeForRange(reader.questionInt("Input first number "), reader.questionInt("Input second number "))));
 
 
 
